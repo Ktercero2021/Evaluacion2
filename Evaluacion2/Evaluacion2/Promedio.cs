da529de27165bd8cf3bdade217f70fee1e35e859
Ktercero2021 <86389488+Ktercero2021@users.noreply.github.com>
2021-07-02 09:04:20 -0600

    primer commit
    primer borrado de codigo

---
+++ Evaluacion2/Evaluacion2/Promedio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluacion2
+{
+    public class Promedio
+    {
+        private string nombre;
+        private int numero_cuenta;
+        private double Nota1;
+        private double nota2;
+        private double nota3;
+        private double nota4;
+
+
+
+    }
+}
